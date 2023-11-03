7eeace45ab06fa30e9db4d78941dcac7af839f17
Chamomile <dltldms5271@gmail.com>
2023-11-04 05:40:42 +0900

    03-1

--- main.c
+++ main.c
@@ -5,19 +5,16 @@
 
 int main(void)
 {
-  int i, average;
-  int sum;
+  int i;
   int grade[SIZE];
-  
-  sum = 0;
+  int score[SIZE];
+
   for(i=0; i<SIZE; i++)
-  {   
-      printf("학생 성적을 입력하세요 :"); 
-      scanf("%d", &grade[i]);
-      sum += grade[i];
-  }
-  average = sum/SIZE;
-  printf("성적 평균 : %d\n", average); 
+      grade[i] = rand()%100;
+  for(i=0; i<SIZE; i++)
+      score[i]=grade[i];
+  for(i=0; i<SIZE; i++)
+      printf("score[%d] = %d\n", i, score[i]);
   
       
   
